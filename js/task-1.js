284be737c9597d58a372f5f58afc6466128e5a46
Fed Kon <fedkon96@gmail.com>
2025-07-08 14:29:25 +0300

    finish

--- js/task-1.js
+++ js/task-1.js
@@ -5,5 +5,6 @@ console.log(`categories = ${liItemEl.length}`);
 liItemEl.forEach(el => {
   const h2El = el.querySelector('h2').textContent;
   const liEl = el.querySelectorAll('ul li').length;
-  console.log(`${h2El} : ${liEl}`);
+  console.log(`Category: ${h2El}`);
+  console.log(`Elements: ${liEl}`);
 });
